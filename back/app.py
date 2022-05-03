c7149af9606ebee4fef8e2ef80600f5c84f0ad18
Romain Heller <Rom1deTroyes@users.noreply.github.com>
2022-05-03 16:00:24 +0200

    ~[App] make it works...
    Signed-off-by: Romain Heller <Rom1deTroyes@users.noreply.github.com>

--- back/app.py
+++ back/app.py
@@ -6,27 +6,39 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    da.connexion()
     etudiants = da.get_etudiants()
-    da.deconnexion()
 
     return render_template("index.html", etudiants=etudiants)
 
-@app.route("/etudiant/<int:id>")
+@app.route("/etudiant/<id>")
 def get_etudiant(id):
-    da.connexion()
     etudiant = da.get_etudiant(id)
-    da.deconnexion()
 
     return render_template("etudiant.html", contenu=etudiant)
 
-@app.route("/etudiant/json/<int:id>")
+@app.route("/etudiant/json/<id>")
 def json_etudiant(id):
-    da.connexion()
     etudiant = da.get_etudiant(id)
-    da.deconnexion()
 
     return jsonify(etudiant), 200
+
+@app.route("/etudiant/inscription")
+def add_etudiant():
+    return render_template("inscription.html")
+
+@app.route("/etudiant/create", methods=['POST'])
+def create_etudiant():
+    nom = request.form["nom"]
+    prenom = request.form["prenom"]
+    age = request.form["age"]
+    classe = request.form["classe"]
+
+    rsp = da.set_etudiant(nom, prenom, age, classe)
+
+    response = jsonify({'requests': 'okay', '_id': rsp})
+    return (response)
+
+    #return  redirect(url_for('index'), code=302)
 
 if __name__ == "__main__" :
     app.run(debug=True)
--- back/data.py
+++ back/data.py
@@ -1,14 +1,16 @@
-from pymongo import MongoClient
+from bson.objectid import ObjectId
+from pymongo.mongo_client import MongoClient
+
 
 class DataAccess :
     user = "root"
-    mdp = "pass12345"
+    mdp = "root"
     db_name = "ecole"
     collection_name = "etudiants"
 
     @classmethod
     def connexion(cls) :
-        cls.client = MongoClient(f"mongodb://{cls.user}:{cls.mdp}@127.0.0.1:27018")
+        cls.client = MongoClient(f"mongodb://{cls.user}:{cls.mdp}@127.0.0.1:27017")
 
         cls.db = cls.client[cls.db_name]
         cls.etudiants = cls.db[cls.collection_name]
@@ -19,18 +21,25 @@ class DataAccess :
 
     @classmethod
     def set_etudiant(cls, nom, prenom, age, classe):
-        taille = cls.etudiants.count_documents({})
-        taille = taille+1
-        
-        etudiant = {"_id":taille, "nom":nom, "prenom":prenom, "age":age, "classe":classe}
-        cls.etudiants.insert_one(etudiant)
+        cls.connexion()
+        etudiant = {"nom":nom, "prenom":prenom, "age":age, "classe":classe}
+        etudiant_id = cls.etudiants.insert_one(etudiant).inserted_id
+        cls.deconnexion()
+        return str(etudiant_id)
+
 
     @classmethod
     def get_etudiants(cls):
-        etudiants = cls.etudiants.find({})
-        return list(etudiants)
+        cls.connexion()
+        etudiants = list(cls.etudiants.find({}))
+        cls.deconnexion()
+        return etudiants
     
     @classmethod
     def get_etudiant(cls, id):
-        etudiant = cls.etudiants.find_one({"_id":id})
+        cls.connexion()
+        #etudiant = cls.etudiants.find_one({"_id":id})
+        etudiant = cls.etudiants.find_one({'_id': ObjectId(id)})
+        cls.deconnexion()
+        etudiant['_id'] = str(etudiant['_id'])
         return etudiant
